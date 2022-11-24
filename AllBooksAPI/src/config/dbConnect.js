3df839216f4a0179717673932a6fd15e2465fe75
Vitor Augusto <vitoraugusto2010201078@gmail.com>
2022-11-24 19:27:39 -0300

    dotenv

--- AllBooksAPI/src/config/dbConnect.js
+++ AllBooksAPI/src/config/dbConnect.js
@@ -2,7 +2,10 @@ import dotenv from "dotenv";
 import mongoose from "mongoose";
 
 
-mongoose.connect("mongodb://vitor:bEfI3M28EeKn4WkJ@-i34q1mv-shard-00-00.ne5b6zo.mongodb.net:27017,ac-i34q1mv-shard-00-01.ne5b6zo.mongodb.net:27017,ac-i34q1mv-shard-00-02.ne5b6zo.mongodb.net:27017/?ssl=true&replicaSet=atlas-80ao85-shard-0&authSource=admin&retryWrites=true&w=majority")
+mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
     .then(() => {
         console.log("Connected to MongoDB");
     })
